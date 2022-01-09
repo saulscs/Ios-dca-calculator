3e418efac035439589e5e9a317f8b0bd34b833b8
Saul Corona <saulcoronascs@gmail.com>
2022-01-09 16:40:02 -0600

    Make a protocol for loading animation

--- Ios-dca-calculator/Controller/SearchTableViewViewController.swift
+++ Ios-dca-calculator/Controller/SearchTableViewViewController.swift
@@ -7,8 +7,9 @@
 
 import UIKit
 import Combine
+import MBProgressHUD
 
-class SearchTableViewViewController: UITableViewController {
+class SearchTableViewViewController: UITableViewController, UIAnimatable {
     
     private enum Mode {
         case onboarding
@@ -53,7 +54,9 @@ class SearchTableViewViewController: UITableViewController {
         $searchQuery
             .debounce(for: .milliseconds(750), scheduler: RunLoop.main)
             .sink{ [unowned self] (searchQuery) in
+                showLoadingAnimation()
                 self.apiService.fetchSymbolsPublisher(keywords: searchQuery).sink { (completion) in
+                    hideLoadingAnimation()
                     switch completion {
                     case .failure(let error):
                         print(error.localizedDescription)
