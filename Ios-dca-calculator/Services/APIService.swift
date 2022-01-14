a4f36f2a046cbe905240e0c650ceabbcc0399721
Saul Corona <saulcoronascs@gmail.com>
2022-01-13 20:20:49 -0600

    Add fetchTimeSerieMonthlyAdjustedToPublisher function to service

--- Ios-dca-calculator/Services/APIService.swift
+++ Ios-dca-calculator/Services/APIService.swift
@@ -36,4 +36,22 @@ struct APIService{
             .receive(on: RunLoop.main)
             .eraseToAnyPublisher()
     }
+    
+    
+    func fetchTimeSerieMonthlyAdjustedToPublisher(keywords: String) -> AnyPublisher<TimeSerieMonthlyAdjusted, Error> {
+        guard let keywords = keywords.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)
+        else { return Fail(error: APIServiceError.encoding).eraseToAnyPublisher()}
+        
+        let urlString = "https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY&symbol=\(keywords)&apikey=\(API_KEY)"
+        
+        guard let url = URL(string: urlString) else { return Fail(error: APIServiceError.badRequest).eraseToAnyPublisher()}
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map({ $0.data })
+            .decode(type: TimeSerieMonthlyAdjusted.self, decoder: JSONDecoder())
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+        
+    }
+    
 }
