608511a0ecf6345a26150542fa9e218a25c3ea98
Saul Corona <saulcoronascs@gmail.com>
2022-01-22 22:08:03 -0600

    get invesment amount

--- Ios-dca-calculator/Services/DCAService.swift
+++ Ios-dca-calculator/Services/DCAService.swift
@@ -10,11 +10,24 @@ import Foundation
 struct DCAService {
     func calcualte(initialInvesmentAmount: Double, monthlyCostAveringAmount: Double, initialDateOfInvesmentIndex: Int) -> DCAResult{
         
+        let investmentAmount = getInvesmentAmount(initialInvesmentAmount: initialInvesmentAmount,
+                                                  monthlyCostAveringAmount: monthlyCostAveringAmount,
+                                                  initialDateOfInvesmentIndex: initialDateOfInvesmentIndex)
+        
+        
         return .init(currentValue: 0,
-                     investmentAmount: 0,
+                     investmentAmount: investmentAmount,
                      gain: 0,
                      yield: 0,
                      annualReturn: 0)
+    }
+    
+    private func getInvesmentAmount(initialInvesmentAmount: Double, monthlyCostAveringAmount: Double, initialDateOfInvesmentIndex: Int) -> Double {
+        var totalAmount = Double()
+        totalAmount += initialInvesmentAmount
+        let dollarCostAveragingAmount = initialDateOfInvesmentIndex.doubleValue * monthlyCostAveringAmount
+        totalAmount += dollarCostAveragingAmount
+        return totalAmount
     }
 }
 
