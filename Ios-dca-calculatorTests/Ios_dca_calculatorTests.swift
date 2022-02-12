78bf1f3881a95b512e933ef34a9945b8a04b6631
Saul Corona <saulcoronascs@gmail.com>
2022-02-12 17:11:32 -0600

    complete first unit test

--- Ios-dca-calculatorTests/Ios_dca_calculatorTests.swift
+++ Ios-dca-calculatorTests/Ios_dca_calculatorTests.swift
@@ -50,7 +50,28 @@ class Ios_dca_calculatorTests: XCTestCase {
                                    monthlyCostAveringAmount: monthlyCostAveringAmount,
                                    initialDateOfInvesmentIndex: initialDateOfInvesmentIndex)
         // then
-        XCTAssertEqual(result.investmentAmount, 12500, "Invesment amount is incorrect")
+        
+        //initial invesment: $5000
+        // DCA: $1500 x 5 = $7500
+        // total: $5000 + $7500 = $12500
+        
+        XCTAssertEqual(result.investmentAmount, 12500)
+        XCTAssertTrue(result.isProfitable)
+        
+        
+        // jan: 5000 / 100 = 50 shares
+        // feb: 1500 / 110 = 13.6363 shares
+        // march: 1500 / 120 = 12.5 shares
+        // april: 1500 / 130 = 11.5384 shares
+        // may: 1500 / 140 = 10.7142
+        // june: 1500 / 150 = 10 shares
+        // total shares = 108.3889
+        // total current value = 108.3889 x 160 (latest month closing price) = $17,342.224
+        
+        
+        XCTAssertEqual(result.currentValue, 29342.257742257745, accuracy: 0.1)
+        XCTAssertEqual(result.gain, 16842.25774257745, accuracy: 0.1)
+        XCTAssertEqual(result.yield, 1.347380619406196, accuracy: 0.001)
     }
     
     func testResult_givenWinnigAssetsAndDCAIsNotUsed_expectPositiveGains(){
@@ -75,12 +96,12 @@ class Ios_dca_calculatorTests: XCTestCase {
     private func buildWinningAsset() -> Asset {
         let searchResult = buildSearchResult()
         let meta = buildMeta()
-        let timesSeries: [String: OHLC] = ["2021-01-25": OHLC(open: "100", close: "0", adjustedClose: "110"),
-                                           "2021-02-25": OHLC(open: "110", close: "0", adjustedClose: "120"),
-                                           "2021-03-25": OHLC(open: "120", close: "0", adjustedClose: "130"),
-                                           "2021-04-25": OHLC(open: "130", close: "0", adjustedClose: "140"),
-                                           "2021-05-25": OHLC(open: "140", close: "0", adjustedClose: "150"),
-                                           "2021-06-25": OHLC(open: "150", close: "0", adjustedClose: "160"),]
+        let timesSeries: [String: OHLC] = ["2021-01-25": OHLC(open: "100", close: "110", adjustedClose: "110"),
+                                           "2021-02-25": OHLC(open: "110", close: "120", adjustedClose: "120"),
+                                           "2021-03-25": OHLC(open: "120", close: "130", adjustedClose: "130"),
+                                           "2021-04-25": OHLC(open: "130", close: "140", adjustedClose: "140"),
+                                           "2021-05-25": OHLC(open: "140", close: "150", adjustedClose: "150"),
+                                           "2021-06-25": OHLC(open: "150", close: "160", adjustedClose: "160"),]
         let timesSeriesMonthlyAdjusted = TimeSerieMonthlyAdjusted(meta: meta, timeSeries: timesSeries)
         
         return Asset(searchResult: searchResult, timesSeriesMonthlyAdjusted: timesSeriesMonthlyAdjusted)
